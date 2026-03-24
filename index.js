30ac87e7ccbeae94671c306af2d99708ca346889
SmoglersPlay <vegacar9917@gmail.com>
2026-03-24 10:13:47 -0700

    Actualización de index.js

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.get('/', (req, res) => {
   res.send('Hola mundo desde Express');
 });
 
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Juan',
+    rol: 'Administrador'
+  };
+  
+  res.json(usuario);
+});
+
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
